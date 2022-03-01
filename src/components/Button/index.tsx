464d15c9125637694d716d6f73c5dddad8ff49df
thibault-barrat <contact@thibault-barrat.com>
2022-03-01 11:24:45 +0100

    add button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,17 @@
+import React, { Children } from "react";
+import styles from "./Button.module.scss";
+
+type Props = {
+  children: React.ReactNode;
+  onClick?: () => void;
+};
+
+const Button = ({ children, onClick }: Props) => {
+  return (
+    <button className={styles.button} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
